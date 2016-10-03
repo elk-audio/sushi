df1df4b13baa95b585fab9c5cb48696b2f5f8040
stez-mind <stefano.zambon@mindmusiclabs.com>
2016-10-03 17:57:54 +0200

    Added SushiBuffer interleave tests

--- test/unittests/engine/engine_test.cpp
+++ test/unittests/engine/engine_test.cpp
@@ -53,3 +53,41 @@ TEST_F(TestEngine, TestProcess)
         ASSERT_FLOAT_EQ(1.0f, buffer.right_out[n]);
     }
 }
+
+TEST(TestSushiBuffer, test_deinterleave_buffer)
+{
+    float interleaved_buffer[AUDIO_CHUNK_SIZE * 2];
+    for (unsigned int n=0; n<AUDIO_CHUNK_SIZE*2; n+=2)
+    {
+        interleaved_buffer[n] = 0.0f;
+        interleaved_buffer[n+1] = 1.0f;
+    }
+    SushiBuffer buffer;
+    buffer.input_from_interleaved(&interleaved_buffer[0]);
+
+    for (unsigned int n=0; n<AUDIO_CHUNK_SIZE; n++)
+    {
+        ASSERT_FLOAT_EQ(0.0f, buffer.left_in[n]);
+        ASSERT_FLOAT_EQ(1.0f, buffer.right_in[n]);
+    }
+
+}
+
+TEST(TestSushiBuffer, test_interleave_buffer)
+{
+    SushiBuffer buffer;
+    for (unsigned int n=0; n<AUDIO_CHUNK_SIZE; n++)
+    {
+        buffer.left_out[n] = 0.0f;
+        buffer.right_out[n] = 1.0f;
+    }
+    float interleaved_buffer[AUDIO_CHUNK_SIZE * 2];
+    buffer.output_to_interleaved(&interleaved_buffer[0]);
+    for (unsigned int n=0; n<AUDIO_CHUNK_SIZE*2; n+=2)
+    {
+        ASSERT_FLOAT_EQ(interleaved_buffer[n], 0.0f);
+        ASSERT_FLOAT_EQ(interleaved_buffer[n+1], 1.0f);
+    }
+    buffer.input_from_interleaved(&interleaved_buffer[0]);
+}
+
