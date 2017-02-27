3076dd232dac41bf3592d13f57b2d88ff29d6db1
Gustav Andersson <gustav.andersson@mindmusiclabs.com>
2017-02-27 15:54:26 +0100

    Complete Midi implementation

--- src/library/midi_decoder.h
+++ src/library/midi_decoder.h
@@ -25,6 +25,7 @@ constexpr int PITCH_BEND_MIDDLE = 8192;
  */
 enum class MessageType
 {
+    /* Channel voice messages */
     NOTE_OFF,
     NOTE_ON,
     POLY_KEY_PRESSURE,
@@ -32,14 +33,31 @@ enum class MessageType
     PROGRAM_CHANGE,
     CHANNEL_PRESSURE,
     PITCH_BEND,
-    CHANNEL_MODE_MSG,
+    /* Channel mode messages */
+    ALL_SOUND_OFF,
+    RESET_ALL_CONTROLLERS,
+    LOCAL_CONTROL_ON,
+    LOCAL_CONTROL_OFF,
+    ALL_NOTES_OFF,
+    OMNI_MODE_OFF,
+    OMNI_MODE_ON,
+    MONO_MODE_ON,
+    POLY_MODE_ON,
+    /* System common messages */
     SYSTEM_EXCLUSIVE,
     TIME_CODE,
     SONG_POSITION,
     SONG_SELECT,
     TUNE_REQUEST,
     END_OF_EXCLUSIVE,
-    UNSUPPORTED,
+    /* System real time messages */
+    TIMING_CLOCK,
+    START,
+    CONTINUE,
+    STOP,
+    ACTIVE_SENSING,
+    RESET,
+    /* Unhandled or corrupt messages */
     UNKNOWN
 };
 
@@ -89,7 +107,21 @@ struct PitchBendMessage
     uint16_t value;
 };
 
+struct TimeCodeMessage
+{
+    uint8_t message_type;
+    uint8_t value;
+};
 
+struct SongPositionMessage
+{
+    uint16_t beats; /* No of beats since start of song, 1 beat = 6 midi clock ticks.*/
+};
+
+struct SongSelectMessage
+{
+    uint8_t index;
+};
 
 /**
  * @brief Decode the type of midi message from a byte string
@@ -98,6 +130,15 @@ struct PitchBendMessage
  * @return The decoded type of data.
  */
 MessageType decode_message_type(const uint8_t* data, size_t size);
+
+/**
+ * @brief Decode the channel number of a channel mode message.
+ *        I.e. when the decoded message type is between
+ *        ALL_SOUND_OFF and POLY_MODE_ON.
+ * @param data First byte of the midi message.
+ * @return The decoded channel number (0-15) from the given message
+ */
+uint8_t decode_channel(const uint8_t data);
 
 /**
  * @brief Decode a midi note off message.
@@ -148,7 +189,30 @@ ChannelPressureMessage decode_channel_pressure(const uint8_t* data);
  */
 PitchBendMessage decode_pitch_bend(const uint8_t* data);
 
+/**
+ * @brief Decode a Midi time code quarter frame message.
+ * @param data  Message data.
+ * @return Decoded message.
+ */
+TimeCodeMessage decode_time_code(const uint8_t* data);
+
+/**
+ * @brief Decode a Midi song position message.
+ * @param data  Message data.
+ * @return Decoded message.
+ */
+SongPositionMessage decode_song_position(const uint8_t* data);
+
+/**
+ * @brief Decode a Midi song select message.
+ * @param data  Message data.
+ * @return Decoded message.
+ */
+SongSelectMessage decode_song_select(const uint8_t* data);
+
+
 } // end namespace midi
 } // end namespace sushi
+
 
 #endif //SUSHI_MIDI_DECODER_H
