5b36765cb935f3498282573d05361a9904c3e237
Gustav Andersson <gustav.andersson@mindmusiclabs.com>
2017-02-28 18:25:52 +0100

    Adding unittest and Jack mockup for it

---
+++ test/unittests/audio_frontends/jack_frontend_test.cpp
@@ -0,0 +1,54 @@
+
+#include "gtest/gtest.h"
+#include <fstream>
+
+#define private public
+#include "jack_mockup.cpp"
+
+#include "audio_frontends/jack_frontend.cpp"
+#include "engine_mockup.h"
+
+
+using ::testing::internal::posix::GetEnv;
+
+using namespace sushi;
+using namespace sushi::audio_frontend;
+
+static constexpr unsigned int SAMPLE_RATE = 44000;
+
+class TestJackFrontend : public ::testing::Test
+{
+protected:
+    TestJackFrontend()
+    {
+    }
+
+    void SetUp()
+    {
+        _module_under_test = new JackFrontend(&_engine);
+    }
+
+    void TearDown()
+    {
+        _module_under_test->cleanup();
+        delete _module_under_test;
+    }
+
+    EngineMockup _engine{SAMPLE_RATE};
+    JackFrontend* _module_under_test;
+};
+
+TEST_F(TestJackFrontend, test_operation)
+{
+    JackFrontendConfiguration config("Jack Client", "Jack Server", false);
+    auto ret_code = _module_under_test->init(&config);
+    ASSERT_EQ(AudioFrontendStatus::OK, ret_code);
+
+    /* Can't call run directly cause that will freeze the test due to the sleep() call*/
+    jack_activate(_module_under_test->_client);
+
+    ASSERT_TRUE(_engine.got_event);
+    ASSERT_TRUE(_engine.process_called);
+}
+
+
---
+++ test/unittests/audio_frontends/jack_mockup.cpp
@@ -0,0 +1,105 @@
+#include <jack/jack.h>
+#include <jack/midiport.h>
+
+/**
+ * @brief Jack mockup that more or less says yes to everything
+ */
+
+constexpr int JACK_NFRAMES = 128;
+uint8_t midi_buffer[3] = {0x81, 60, 45};
+float buffer[JACK_NFRAMES];
+
+struct _jack_port
+{
+    int no{0};
+};
+
+struct _jack_client
+{
+    JackProcessCallback callback_function;
+    void* instance;
+    _jack_port mocked_ports[10];
+};
+
+
+jack_client_t * jack_client_open (const char* /*client_name*/,
+                                  jack_options_t /*options*/,
+                                  jack_status_t* status, ...)
+{
+    *status = JackClientZombie; /* I am zombie client! */
+    return new jack_client_t;
+}
+
+int jack_client_close (jack_client_t *client)
+{
+    delete client;
+    return 0;
+}
+
+jack_port_t * jack_port_register (jack_client_t* client,
+                                  const char* /*port_name*/,
+                                  const char* /*port_type*/,
+                                  unsigned long /*flags*/,
+                                  unsigned long /*buffer_size*/)
+{
+    static int i = 0;
+    return &client->mocked_ports[i++];
+}
+
+int jack_set_process_callback (jack_client_t* client,
+                               JackProcessCallback process_callback,
+                               void *arg)
+{
+    client->instance = arg;
+    client->callback_function = process_callback;
+    return 0;
+}
+
+int jack_activate (jack_client_t* client)
+{
+    client->callback_function(JACK_NFRAMES, client->instance);
+    return 0;
+}
+
+void * jack_port_get_buffer (jack_port_t* /*port*/, jack_nframes_t)
+{
+    return buffer;
+}
+
+uint32_t jack_midi_get_event_count(void* /*port_buffer*/)
+{
+    return 1;
+}
+
+int jack_midi_event_get(jack_midi_event_t *event,
+                        void* /*port_buffer*/,
+                        uint32_t /*event_index*/)
+{
+    event->size = 3;
+    event->buffer = midi_buffer;
+    return 0;
+}
+
+/* Functions below are only added for completion, not implemented
+ * and shouldn't be called*/
+const char ** jack_get_ports (jack_client_t* /*client*/,
+                              const char* /*port_name_pattern*/,
+                              const char* /*type_name_pattern*/,
+                              unsigned long /*flags*/)
+{
+    return nullptr;
+}
+
+int jack_connect (jack_client_t* /*client*/,
+                  const char* /*source_port*/,
+                  const char* /*destination_port*/)
+{
+    return 0;
+}
+
+const char * jack_port_name (const jack_port_t* /*port*/)
+{
+    return nullptr;
+}
+
+void jack_free(void* /*ptr*/) {}
