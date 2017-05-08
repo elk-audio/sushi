f81ab8813bd7c19f81ff307978ff1be51ea185e9
Gustav Andersson <gustav.andersson@mindmusiclabs.com>
2017-05-08 12:21:59 +0200

    Making Plugin Events trivially copyable

--- src/library/plugin_events.h
+++ src/library/plugin_events.h
@@ -34,33 +34,24 @@ enum class EventType
 class BaseEvent
 {
 public:
-    virtual ~BaseEvent() {}
-
     /**
      * @brief Type of event.
      * @return
      */
-    EventType type() { return _type;};
+    EventType type() const {return _type;};
 
     /**
      * @brief The processor id of the target for this message.
      * @return
      */
-    ObjectId processor_id() {return _processor_id;}
-
-    /**
-     * @brief If the message should be handled in the realtime context or not
-     *        Maybe not neccesary.
-     * @return
-     */
-    virtual bool is_real_time() = 0;
+    ObjectId processor_id() const {return _processor_id;}
 
     /**
      * @brief For real time events that need sample accurate timing, how many
      *        samples into the current chunk the event should take effect.
      * @return
      */
-    int sample_offset() {return _sample_offset;}
+    int sample_offset() const {return _sample_offset;}
 
 protected:
     BaseEvent(EventType type, ObjectId target, int offset) : _type(type),
@@ -85,9 +76,8 @@ public:
                type == EventType::NOTE_OFF ||
                type == EventType::NOTE_AFTERTOUCH);
     }
-    bool is_real_time() override { return true;}
-    int note() {return _note;}
-    float velocity() {return _velocity;}
+    int note() const {return _note;}
+    float velocity() const {return _velocity;}
 
 protected:
     int _note;
@@ -103,20 +93,15 @@ class WrappedMidiEvent : public BaseEvent
 {
 public:
     WrappedMidiEvent(int offset, ObjectId target, uint8_t byte_0, uint8_t byte_1, uint8_t byte_2) : BaseEvent(EventType::WRAPPED_MIDI_EVENT, target, offset),
-                                                                                                    _midi_byte_0(byte_0),
-                                                                                                    _midi_byte_1(byte_1),
-                                                                                                    _midi_byte_2(byte_2) {}
+                                                                                                    _midi_data{byte_0, byte_1, byte_2} {}
 
-    uint8_t midi_byte_0() {return _midi_byte_0;}
-    uint8_t midi_byte_1() {return _midi_byte_1;}
-    uint8_t midi_byte_2() {return _midi_byte_2;}
-
-    bool is_real_time() override { return true;}
+    const uint8_t* midi_data() const {return _midi_data;}
+    uint8_t midi_byte_0() const {return _midi_data[0];}
+    uint8_t midi_byte_1() const {return _midi_data[1];}
+    uint8_t midi_byte_2() const {return _midi_data[2];}
 
 protected:
-    uint8_t _midi_byte_0;
-    uint8_t _midi_byte_1;
-    uint8_t _midi_byte_2;
+    uint8_t _midi_data[3];
 };
 /**
  * @brief Baseclass for simple parameter changes
@@ -132,11 +117,9 @@ public:
                type == EventType::INT_PARAMETER_CHANGE ||
                type == EventType::BOOL_PARAMETER_CHANGE);
     }
-    ObjectId param_id() {return _param_id;}
+    ObjectId param_id() const {return _param_id;}
 
-    float value(){return _value;}
-
-    bool is_real_time() override { return true;}
+    float value() const {return _value;}
 
 protected:
     ObjectId _param_id;
@@ -152,7 +135,7 @@ public:
     DataPayloadEvent(EventType type, ObjectId processor, int offset, void* data) : BaseEvent(type, processor, offset),
                                                                                    _data(data) {}
 
-    void* data() {return _data;}
+    void* data() const {return _data;}
 
     /**
      * @brief Call to mark the event as handled. Data can not be accessed after this.
@@ -165,7 +148,7 @@ public:
      * Returns true if the event is handled and the data is ok to delete.
      * @return
      */
-    bool handled() {return _handled;}
+    bool handled() const {return _handled;}
 
 protected:
     void* _data;
@@ -187,11 +170,9 @@ public:
                                                                       static_cast<void*>(value)),
                                                      _param_id(param_id) {}
 
-    ObjectId param_id() {return _param_id;}
+    ObjectId param_id() const {return _param_id;}
 
-    std::string* value(){return static_cast<std::string*>(_data);}
-
-    bool is_real_time() override { return true;}
+    std::string* value() const {return static_cast<std::string*>(_data);}
 
 protected:
     ObjectId _param_id;
@@ -206,25 +187,125 @@ public:
     DataParameterChangeEvent(ObjectId processor,
                              int offset,
                              ObjectId param_id,
-                             char* value) : DataPayloadEvent(EventType::DATA_PARAMETER_CHANGE,
+                             void* value) : DataPayloadEvent(EventType::DATA_PARAMETER_CHANGE,
                                                              processor,
                                                              offset,
                                                              static_cast<void*>(value)),
                                             _param_id(param_id) {}
 
-    ObjectId param_id() { return _param_id;}
+    ObjectId param_id() const {return _param_id;}
 
-    char* value(){return static_cast<char*>(_data);}
-
-    bool is_real_time() override { return true;}
+    char* value() const {return static_cast<char*>(_data);}
 
 protected:
     ObjectId _param_id;
 };
 
-/* TODO replace this with our own iterable container class or wrapper.*/
 
-typedef std::vector<BaseEvent*> EventList;
+/**
+ * @brief Container class for events
+ */
+class Event
+{
+public:
+    EventType type() {return _keyboard_event.type();}
+
+    const KeyboardEvent& keyboard_event()
+    {
+        assert(_keyboard_event.type() == EventType::NOTE_ON ||
+               _keyboard_event.type() == EventType::NOTE_OFF ||
+               _keyboard_event.type() == EventType::NOTE_AFTERTOUCH);
+        return _keyboard_event;
+    }
+
+    const WrappedMidiEvent& wrapper_midi_event() const
+    {
+        assert(_wrapped_midi_event.type() == EventType::WRAPPED_MIDI_EVENT);
+        return _wrapped_midi_event;
+    }
+
+    const ParameterChangeEvent& parameter_change_event() const
+    {
+        assert(_keyboard_event.type() == EventType::FLOAT_PARAMETER_CHANGE);
+        return _parameter_change_event;
+    }
+    const StringParameterChangeEvent& string_parameter_change_event() const
+    {
+        assert(_string_parameter_change_event.type() == EventType::STRING_PARAMETER_CHANGE);
+        return _string_parameter_change_event;
+    }
+    const DataParameterChangeEvent& data_parameter_change_event() const
+    {
+        assert(_data_parameter_change_event.type() == EventType::DATA_PARAMETER_CHANGE);
+        return _data_parameter_change_event;
+    }
+
+    // Factory functions for constructing
+    static Event make_note_on_event(ObjectId target, int offset, int note, float velocity)
+    {
+        return make_keyboard_event(EventType::NOTE_ON, target, offset, note, velocity);
+    }
+
+    static Event make_note_off_event(ObjectId target, int offset, int note, float velocity)
+    {
+        return make_keyboard_event(EventType::NOTE_OFF, target, offset, note, velocity);
+    }
+
+    static Event make_note_aftertouch_event(ObjectId target, int offset, int note, float velocity)
+    {
+        return make_keyboard_event(EventType::NOTE_AFTERTOUCH, target, offset, note, velocity);
+    }
+
+    static Event make_keyboard_event(EventType type, ObjectId target, int offset, int note, float velocity)
+    {
+        KeyboardEvent typed_event(type, target, offset, note, velocity);
+        return Event(typed_event);
+    }
+
+    static Event make_parameter_change_event(ObjectId target, int offset, ObjectId param_id, float value)
+    {
+        ParameterChangeEvent typed_event(EventType::FLOAT_PARAMETER_CHANGE, target, offset, param_id, value);
+        return Event(typed_event);
+    }
+
+    static Event make_wrapped_midi_event(ObjectId target, int offset,  uint8_t byte_0, uint8_t byte_1, uint8_t byte_2)
+    {
+        WrappedMidiEvent typed_event(offset, target, byte_0, byte_1, byte_2);
+        return Event(typed_event);
+    }
+
+    static Event make_string_parameter_change_event(ObjectId target, int offset, ObjectId param_id, std::string* value)
+    {
+        StringParameterChangeEvent typed_event(target, offset, param_id, value);
+        return Event(typed_event);
+    }
+
+    static Event make_data_parameter_change_event(ObjectId target, int offset, ObjectId param_id, void* data)
+    {
+        DataParameterChangeEvent typed_event(target, offset, param_id, data);
+        return Event(typed_event);
+    }
+
+private:
+    Event(const KeyboardEvent& e) : _keyboard_event(e) {}
+    Event(const ParameterChangeEvent& e) : _parameter_change_event(e) {}
+    Event(const WrappedMidiEvent& e) : _wrapped_midi_event(e) {}
+    Event(const StringParameterChangeEvent& e) : _string_parameter_change_event(e) {}
+    Event(const DataParameterChangeEvent& e) : _data_parameter_change_event(e) {}
+    union
+    {
+        KeyboardEvent               _keyboard_event;
+        WrappedMidiEvent            _wrapped_midi_event;
+        ParameterChangeEvent        _parameter_change_event;
+        StringParameterChangeEvent  _string_parameter_change_event;
+        DataParameterChangeEvent    _data_parameter_change_event;
+    };
+} __attribute__ ((aligned (32)));
+
+static_assert(sizeof(Event) == 32, "");
+static_assert(std::is_trivially_copyable<Event>::value, "");
+
+
 
 } // namespace sushi
 
