3d1bb70dfb8bfb5d6bd08217231965a91660f042
sharanrao <sharan.yagneswar@mindmusiclabs.com>
2017-05-24 11:37:50 +0200

    Removed all related json headers, methods and helper functions from engine and engine test

--- test/unittests/engine/engine_test.cpp
+++ test/unittests/engine/engine_test.cpp
@@ -14,31 +14,9 @@
 
 using ::testing::internal::posix::GetEnv;
 
-
 constexpr unsigned int SAMPLE_RATE = 44000;
 using namespace sushi;
 using namespace sushi::engine;
-
-Json::Value read_json_config(const std::string& file_name)
-{
-    char const* test_data_dir = GetEnv("SUSHI_TEST_DATA_DIR");
-    if (test_data_dir == nullptr)
-    {
-        EXPECT_TRUE(false) << "Can't access Test Data environment variable\n";
-    }
-    std::string test_config_file(test_data_dir);
-    test_config_file.append("/" + file_name);
-
-    std::ifstream file(test_config_file);
-    Json::Value config;
-    Json::Reader reader;
-    bool parse_ok = reader.parse(file, config, false);
-    if (!parse_ok)
-    {
-        EXPECT_TRUE(false) << "Error parsing JSON config file\n";
-    }
-    return config;
-};
 
 /*
 * Engine tests
@@ -87,25 +65,11 @@ TEST_F(TestEngine, TestProcess)
     test_utils::assert_buffer_value(1.0f, out_buffer);
 }
 
-TEST_F(TestEngine, TestInitMidiFromJSON)
-{
-    Json::Value config = read_json_config("config.json");
-    ASSERT_FALSE(config.empty());
-    _module_under_test->set_midi_input_ports(1);
-    EngineReturnStatus status = _module_under_test->init_chains_from_json_array(config["stompbox_chains"]);
-    ASSERT_EQ(EngineReturnStatus::OK, status);
-    status = _module_under_test->init_midi_from_json_array(config["midi"]);
-    ASSERT_EQ(EngineReturnStatus::OK, status);
-
-    ASSERT_EQ(1u, _module_under_test->_midi_dispatcher._kb_routes.size());
-    ASSERT_EQ(1u, _module_under_test->_midi_dispatcher._cc_routes.size());
-}
-
 TEST_F(TestEngine, TestUidNameMapping)
 {
-    Json::Value config = read_json_config("config.json");
-    ASSERT_FALSE(config.empty());
-    EngineReturnStatus status = _module_under_test->init_chains_from_json_array(config["stompbox_chains"]);
+    auto status = _module_under_test->create_plugin_chain("left",2);
+    ASSERT_EQ(EngineReturnStatus::OK, status);
+    status = _module_under_test->add_plugin_to_chain("left", "sushi.testing.gain", "gain_0_l");
     ASSERT_EQ(EngineReturnStatus::OK, status);
 
     ObjectId id;
@@ -125,9 +89,11 @@ TEST_F(TestEngine, TestUidNameMapping)
 
 TEST_F(TestEngine, TestParameterLookup)
 {
-    Json::Value config = read_json_config("config.json");
-    ASSERT_FALSE(config.empty());
-    EngineReturnStatus status = _module_under_test->init_chains_from_json_array(config["stompbox_chains"]);
+    auto status = _module_under_test->create_plugin_chain("left", 2);
+    ASSERT_EQ(EngineReturnStatus::OK, status);
+    status = _module_under_test->add_plugin_to_chain("left", "sushi.testing.equalizer", "equalizer_0_l");
+    ASSERT_EQ(EngineReturnStatus::OK, status);
+    status = _module_under_test->add_plugin_to_chain("left", "sushi.testing.gain", "gain_0_l");
     ASSERT_EQ(EngineReturnStatus::OK, status);
 
     ObjectId id;
@@ -143,10 +109,11 @@ TEST_F(TestEngine, TestParameterLookup)
 
 TEST_F(TestEngine, TestProcessorExist)
 {
-    Json::Value config = read_json_config("config.json");
-    ASSERT_FALSE(config.empty());
-    EngineReturnStatus status = _module_under_test->init_chains_from_json_array(config["stompbox_chains"]);
+    auto status = _module_under_test->create_plugin_chain("left", 2);
     ASSERT_EQ(EngineReturnStatus::OK, status);
+    status = _module_under_test->add_plugin_to_chain("left", "sushi.testing.passthrough", "passthrough_0_l");
+    ASSERT_EQ(EngineReturnStatus::OK, status);
+    status = _module_under_test->add_plugin_to_chain("left", "sushi.testing.gain", "gain_1_r");
 
     /* Test by passing processor names as arguments */
     ASSERT_TRUE(_module_under_test->_processor_exists("passthrough_0_l"));
