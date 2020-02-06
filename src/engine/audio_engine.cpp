fa6ed9c2cf3332f390feafceb46ab6c648856fcb
Gustav Andersson <gustav@elk.audio>
2020-02-06 14:57:53 +0100

    Formatting fixes

--- src/engine/audio_engine.cpp
+++ src/engine/audio_engine.cpp
@@ -478,10 +478,10 @@ bool AudioEngine::_remove_processor_from_realtime_part(ObjectId processor)
     return true;
 }
 
-void AudioEngine::process_chunk(SampleBuffer <AUDIO_CHUNK_SIZE>*in_buffer,
-                                SampleBuffer <AUDIO_CHUNK_SIZE>*out_buffer,
-                                ControlBuffer*in_controls,
-                                ControlBuffer*out_controls,
+void AudioEngine::process_chunk(SampleBuffer<AUDIO_CHUNK_SIZE>* in_buffer,
+                                SampleBuffer<AUDIO_CHUNK_SIZE>* out_buffer,
+                                ControlBuffer* in_controls,
+                                ControlBuffer* out_controls,
                                 Time timestamp,
                                 int64_t samplecount)
 {
