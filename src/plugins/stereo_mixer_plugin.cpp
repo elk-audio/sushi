8f7a2fb12914051722196232a0763f90edb32308
Ruben Svensson <ruben@elk.audio>
2021-07-12 17:56:53 +0200

    Implemented main processing

--- src/plugins/stereo_mixer_plugin.cpp
+++ src/plugins/stereo_mixer_plugin.cpp
@@ -1,14 +1,36 @@
+#include <iostream>
+
 #include "stereo_mixer_plugin.h"
 
 namespace sushi {
 namespace stereo_mixer_plugin {
+
+namespace {
+    constexpr float PAN_GAIN_3_DB = 1.412537f;
+
+    inline std::pair<float, float> calc_l_r_gain(float gain, float pan)
+    {
+         float left_gain, right_gain;
+        if (pan < 0.0f) // Audio panned left
+        {
+            left_gain = gain * (1.0f + pan - PAN_GAIN_3_DB * pan);
+            right_gain = gain * (1.0f + pan);
+        }
+        else            // Audio panned right
+        {
+            left_gain = gain * (1.0f - pan);
+            right_gain = gain * (1.0f - pan + PAN_GAIN_3_DB * pan);
+        }
+        return {left_gain / PAN_GAIN_3_DB, right_gain / PAN_GAIN_3_DB};
+    }
+}
 
 StereoMixerPlugin::StereoMixerPlugin(HostControl HostControl): InternalPlugin(HostControl)
 {
     _max_input_channels = MAX_CHANNELS_SUPPORTED;
     _max_output_channels = MAX_CHANNELS_SUPPORTED;
     Processor::set_name(DEFAULT_NAME);
-    Processor::set_name(DEFAULT_LABEL);
+    Processor::set_label(DEFAULT_LABEL);
 
     _left_pan = register_float_parameter("left_pan", "Left Pan", "",
                                          -1.0, -1.0, 1.0, nullptr);
@@ -33,7 +55,21 @@ StereoMixerPlugin::StereoMixerPlugin(HostControl HostControl): InternalPlugin(Ho
 void StereoMixerPlugin::process_audio(const ChunkSampleBuffer& input_buffer,
                                      ChunkSampleBuffer& output_buffer)
 {
-    output_buffer.add(input_buffer);
+    output_buffer.clear();
+
+    // Calculate parameters
+    float invert_ch1 = (_left_invert_phase->processed_value() > 0.5f) ? -1.0f : 1.0f;
+    auto [ch1_left_gain, ch1_right_gain] = calc_l_r_gain(_left_gain->processed_value() * invert_ch1,
+                                                         _left_pan->processed_value());
+    float invert_ch2 = (_right_invert_phase->processed_value() > 0.5f) ? -1.0f : 1.0f;
+    auto [ch2_left_gain, ch2_right_gain] = calc_l_r_gain(_right_gain->processed_value() * invert_ch2,
+                                                         _right_pan->processed_value());
+
+    // Process gain
+    output_buffer.add_with_gain(0, 0, input_buffer, ch1_left_gain);
+    output_buffer.add_with_gain(1, 0, input_buffer, ch1_right_gain);
+    output_buffer.add_with_gain(0, 1, input_buffer, ch2_left_gain);
+    output_buffer.add_with_gain(1, 1, input_buffer, ch2_right_gain);
 }
 
 } // namespace stereo_mixer_plugin
--- test/unittests/plugins/plugins_test.cpp
+++ test/unittests/plugins/plugins_test.cpp
@@ -601,7 +601,7 @@ TEST_F(TestStereoMixerPlugin, TestInitialization)
 {
     _module_under_test->init(TEST_SAMPLERATE);
     ASSERT_TRUE(_module_under_test);
-    ASSERT_EQ("Stereo mixer", _module_under_test->label());
+    ASSERT_EQ("Stereo Mixer", _module_under_test->label());
     ASSERT_EQ("sushi.testing.stereo_mixer", _module_under_test->name());
 }
 
@@ -621,7 +621,7 @@ TEST_F(TestStereoMixerPlugin, TestProcess)
 
     // Standard stereo throughput, right input channel inverted
 
-    _module_under_test->_left_pan->set(-1.0f);
+    _module_under_test->_left_pan->set(0.0f);
     _module_under_test->_left_gain->set(0.5f);
     _module_under_test->_left_invert_phase->set(0.0f);
     _module_under_test->_right_pan->set(1.0f);
@@ -639,7 +639,7 @@ TEST_F(TestStereoMixerPlugin, TestProcess)
     _module_under_test->_left_pan->set(1.0f);
     _module_under_test->_left_gain->set(0.7f);
     _module_under_test->_left_invert_phase->set(1.0f);
-    _module_under_test->_right_pan->set(-1.0f);
+    _module_under_test->_right_pan->set(0.0f);
     _module_under_test->_right_gain->set(0.3f);
     _module_under_test->_right_invert_phase->set(0.0f);
 
@@ -650,29 +650,29 @@ TEST_F(TestStereoMixerPlugin, TestProcess)
     test_utils::compare_buffers<AUDIO_CHUNK_SIZE>(output_buffer, expected_buffer, 2);
 
     // Mono summing
-    _module_under_test->_left_pan->set(0.0f);
+    _module_under_test->_left_pan->set(0.5f);
     _module_under_test->_left_gain->set(1.0f);
     _module_under_test->_left_invert_phase->set(0.0f);
-    _module_under_test->_right_pan->set(0.0f);
+    _module_under_test->_right_pan->set(0.5f);
     _module_under_test->_right_gain->set(1.0f);
     _module_under_test->_right_invert_phase->set(0.0f);
 
-    std::fill(expected_buffer.channel(0), expected_buffer.channel(0) + AUDIO_CHUNK_SIZE, -1.0f);
-    std::fill(expected_buffer.channel(1), expected_buffer.channel(1) + AUDIO_CHUNK_SIZE, -1.0f);
+    std::fill(expected_buffer.channel(0), expected_buffer.channel(0) + AUDIO_CHUNK_SIZE, -0.707946f);
+    std::fill(expected_buffer.channel(1), expected_buffer.channel(1) + AUDIO_CHUNK_SIZE, -0.707946f);
 
     _module_under_test->process_audio(input_buffer, output_buffer);
     test_utils::compare_buffers<AUDIO_CHUNK_SIZE>(output_buffer, expected_buffer, 2);
 
     // Pan law test
-    _module_under_test->_left_pan->set(-0.3f);
+    _module_under_test->_left_pan->set(0.35f);
     _module_under_test->_left_gain->set(1.0f);
     _module_under_test->_left_invert_phase->set(0.0f);
-    _module_under_test->_right_pan->set(0.8f);
+    _module_under_test->_right_pan->set(0.9f);
     _module_under_test->_right_gain->set(1.0f);
     _module_under_test->_right_invert_phase->set(0.0f);
 
-    std::fill(expected_buffer.channel(0), expected_buffer.channel(0) + AUDIO_CHUNK_SIZE, 1.1237640937f + -3.6f);
-    std::fill(expected_buffer.channel(1), expected_buffer.channel(1) + AUDIO_CHUNK_SIZE, 0.7f + -5.8600751664f);
+    std::fill(expected_buffer.channel(0), expected_buffer.channel(0) + AUDIO_CHUNK_SIZE, 0.7955587392184001f + -0.28317642241051433f);
+    std::fill(expected_buffer.channel(1), expected_buffer.channel(1) + AUDIO_CHUNK_SIZE, 0.49555873921840016f + -1.8831764224105143f);
 
     _module_under_test->process_audio(input_buffer, output_buffer);
     test_utils::compare_buffers<AUDIO_CHUNK_SIZE>(output_buffer, expected_buffer, 2);
