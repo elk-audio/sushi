078704d8ef6fba8a48b27b9684ae4ad1584f3140
Gustav Andersson <gustav@elk.audio>
2022-05-02 14:56:52 +0200

    Updating Ableton Link to v3.0.5

--- src/engine/transport.h
+++ src/engine/transport.h
@@ -49,6 +49,12 @@ enum class PlayStateChange
 
 namespace engine {
 
+#if SUSHI_BUILD_WITH_ABLETON_LINK
+class SushiLink;
+#else
+using SushiLink = ::ableton::Link;
+#endif
+
 constexpr float DEFAULT_TEMPO = 120;
 
 class Transport
@@ -241,7 +247,7 @@ private:
 
     RtEventPipe*    _rt_event_dispatcher;
 
-    std::unique_ptr<ableton::Link>  _link_controller;
+    std::unique_ptr<SushiLink>  _link_controller;
 };
 
 } // namespace engine
