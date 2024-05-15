7c37860421a8d1ef2cf356e1f47aac8e7105dc4d
Ruben Svensson <ruben@elk.audio>
2024-05-15 16:09:01 +0200

    Use cmake generator expression to get vst3 test plugin path

--- test/unittests/library/vst3x_wrapper_test.cpp
+++ test/unittests/library/vst3x_wrapper_test.cpp
@@ -64,12 +64,6 @@ private:
 using namespace sushi;
 using namespace sushi::internal::vst3;
 
-#ifdef NDEBUG
-const char PLUGIN_FILE[] = "../VST3/Release/adelay.vst3";
-#else
-const char PLUGIN_FILE[] = "../VST3/Debug/adelay.vst3";
-#endif
-
 const char PLUGIN_NAME[] = "ADelay";
 
 constexpr unsigned int DELAY_PARAM_ID = 100;
@@ -80,7 +74,7 @@ constexpr int   TEST_CHANNEL_COUNT = 2;
 /* Quick test to test plugin loading */
 TEST(TestVst3xPluginInstance, TestLoadPlugin)
 {
-    auto full_path = std::filesystem::path(PLUGIN_FILE);
+    auto full_path = std::filesystem::path(SUSHI_VST3_TEST_PLUGIN_PATH);
     auto full_test_plugin_path = std::string(std::filesystem::absolute(full_path));
 
     SushiHostApplication host_app;
@@ -102,7 +96,7 @@ TEST(TestVst3xPluginInstance, TestLoadPluginFromErroneousFilename)
     ASSERT_FALSE(success);
 
     /* Existing library but non-existing plugin */
-    auto full_path = std::filesystem::path(PLUGIN_FILE);
+    auto full_path = std::filesystem::path(SUSHI_VST3_TEST_PLUGIN_PATH);
     auto full_test_plugin_path = std::string(std::filesystem::absolute(full_path));
 
     success = module_under_test.load_plugin(full_test_plugin_path, "NoPluginWithThisName");
@@ -177,12 +171,12 @@ protected:
 
 TEST_F(TestVst3xWrapper, TestLoadAndInitPlugin)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
     ASSERT_TRUE(_module_under_test.get());
     EXPECT_EQ("ADelay", _module_under_test->name());
 
     auto parameters = _module_under_test->all_parameters();
-    EXPECT_EQ(1u, parameters.size());
+    ASSERT_EQ(1u, parameters.size());
     EXPECT_EQ("Delay", parameters[0]->name());
     EXPECT_EQ(DELAY_PARAM_ID, parameters[0]->id());
     EXPECT_TRUE(_accessor->bypass_parameter().supported);
@@ -202,7 +196,7 @@ TEST_F(TestVst3xWrapper, TestLoadAndInitPlugin)
 
 TEST_F(TestVst3xWrapper, TestProcessing)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
     ChunkSampleBuffer in_buffer(2);
     ChunkSampleBuffer out_buffer(2);
     test_utils::fill_sample_buffer(in_buffer, 1);
@@ -222,7 +216,7 @@ TEST_F(TestVst3xWrapper, TestProcessing)
 
 TEST_F(TestVst3xWrapper, TestBypassProcessing)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
     ChunkSampleBuffer in_buffer(2);
     ChunkSampleBuffer out_buffer(2);
     test_utils::fill_sample_buffer(in_buffer, 1.0f);
@@ -249,7 +243,7 @@ TEST_F(TestVst3xWrapper, TestBypassProcessing)
 
 TEST_F(TestVst3xWrapper, TestEventForwarding)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
 
     Steinberg::Vst::Event note_on_event;
     note_on_event.type = Steinberg::Vst::Event::EventTypes::kNoteOnEvent;
@@ -288,14 +282,14 @@ TEST_F(TestVst3xWrapper, TestEventForwarding)
 
 TEST_F(TestVst3xWrapper, TestConfigurationChange)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
     _module_under_test->configure(44100.0f);
     ASSERT_FLOAT_EQ(44100, _accessor->sample_rate());
 }
 
 TEST_F(TestVst3xWrapper, TestTimeInfo)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
     _host_control._transport.set_playing_mode(PlayingMode::PLAYING, false);
     _host_control._transport.set_tempo(120, false);
     _host_control._transport.set_time_signature({3, 4}, false);
@@ -322,7 +316,7 @@ TEST_F(TestVst3xWrapper, TestParameterHandling)
 {
     ChunkSampleBuffer in_buffer(2);
     ChunkSampleBuffer out_buffer(2);
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
 
     auto [status, value] = _module_under_test->parameter_value(DELAY_PARAM_ID);
     EXPECT_EQ(ProcessorReturnCode::OK, status);
@@ -346,7 +340,7 @@ TEST_F(TestVst3xWrapper, TestParameterHandling)
 
 TEST_F(TestVst3xWrapper, TestGateOutput)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
 
     auto status = _module_under_test->connect_gate_from_processor(2, 0, 46);
     ASSERT_EQ(ProcessorReturnCode::OK, status);
@@ -373,7 +367,7 @@ TEST_F(TestVst3xWrapper, TestGateOutput)
 
 TEST_F(TestVst3xWrapper, TestCVOutput)
 {
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
 
     auto status = _module_under_test->connect_cv_from_parameter(DELAY_PARAM_ID, 1);
     ASSERT_EQ(ProcessorReturnCode::OK, status);
@@ -399,7 +393,7 @@ TEST_F(TestVst3xWrapper, TestCVOutput)
 TEST_F(TestVst3xWrapper, TestStateHandling)
 {
     ChunkSampleBuffer buffer(2);
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
 
     auto desc = _module_under_test->parameter_from_name("Delay");
     ASSERT_TRUE(desc);
@@ -460,7 +454,7 @@ TEST_F(TestVst3xWrapper, TestStateHandling)
 TEST_F(TestVst3xWrapper, TestMultipleStates)
 {
     ChunkSampleBuffer buffer(2);
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
 
     auto desc = _module_under_test->parameter_from_name("Delay");
     ASSERT_TRUE(desc);
@@ -521,7 +515,7 @@ TEST_F(TestVst3xWrapper, TestMultipleStates)
 TEST_F(TestVst3xWrapper, TestBinaryStateSaving)
 {
     ChunkSampleBuffer buffer(2);
-    SetUp(PLUGIN_FILE, PLUGIN_NAME);
+    SetUp(SUSHI_VST3_TEST_PLUGIN_PATH, PLUGIN_NAME);
 
     auto desc = _module_under_test->parameter_from_name("Delay");
     ASSERT_TRUE(desc);
